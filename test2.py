f957305bf3f592d1195bd1987876b36891abf543
egorvitalich <egor.sirota2018@gmail.com>
2026-04-09 00:26:26 +0700

    happy path

---
+++ test2.py
@@ -0,0 +1,31 @@
+TASKS = [
+    {
+        "name": "Salary_cards",  # Зарплатные карты
+        "url": "https://www.banki.ru/products/debitcards/catalogue/zarplatnyie_kartyi/",
+        "keys": ["Размер кэшбэка", "Процент на остаток", "Привилегии для держателей", "Накопительный счет", ]
+    },
+    {
+        "name": "Savings_accounts",  # Накопительные счета
+        "url": "https://www.banki.ru/products/deposits/catalogue/nakopitelnyie_scheta/",
+        "keys": ["Ставка c 1 месяца", "Ставка c 3 месяца", "Частичное снятие", "Пополнение", "Выплата процентов",
+                 "Досрочное расторжение", "срок "]
+    },
+    {
+        "name": "Interest_on_balance",  # Процент на остаток
+        "url": "https://www.banki.ru/products/debitcards/catalogue/_debetovyie_kartyi_s_protsentom__na_ostatok/",
+        "keys": ["Размер кэшбэка", "Процент на остаток", "Накопительный счет", "Привилегии для держателей", "Баллы"]
+    },
+    {
+        "name": "Salary_projects",  # Зарплатные проекты
+        "url": "https://www.sravni.ru/biznes/zarplatnyj-proekt/",
+        "keys": ["Подключение", "Минимальное количество сотрудников", "Комиссия за начисление на карты других банков",
+                 "Работает с 1С", "Преимущества для организации", "Дополнительные преимущества",
+                 "Преимущества для сотрудников",
+                 "Преимущества карты сотрудника", ]
+    },
+    {
+        "name": "Investment_money_boxes",  # Инвесткопилки
+        "url": "https://evaprodengi.ru/2025/08/07/kak-rabotaet-investkopilka-i-kakaya-iz-nih-luchshe/",
+        "keys": ["Плюсы", "Минусы"]
+    }
+]
